abe20249a493b2e8fd6d66501b59fa53eae9b5e6
combermere246 <combermere246@users.noreply.github.com>
2018-09-19 12:55:48 -0400

    mystery.cpp

--- mystery.cpp
+++ mystery.cpp
@@ -14,17 +14,45 @@ void mystery1(auto& D)
 {
   cout<<endl<<"Mystery 1"<<endl<<"---------------------"<<endl;
 
-  for ( int i = 0 ; i < Data.size( ) ; i++)
+  for (unsigned int i = 0 ; i < D.size( ) ; i++)
   {
-    for ( int j = 0 ; j < i ; j++)
-	if ( Data[ i ] < Data[ j ] )
-	    swap( Data[ i ] , Data[ j ] );
+    for (unsigned int j = 0 ; j < i ; j++)
+	if ( D[ i ] < D[ j ] )
+	    swap( D[ i ] , D[ j ] );
 
     print(D);
   }//end outer for (this brace is needed to include the print statement)
 
 }
 
+void mystery2(auto& D)
+{
+	cout<<endl<<"Mystery 2"<<endl<<"---------------------"<<endl;
+	
+	for (unsigned int i = 0; i < D.size(); i++)
+	{
+	for (unsigned int j = 0; j < D.size() - 1; j++)
+	if (D[j+1] < D[j])
+		swap (D[j+1], D[j]);
+		
+		print(D);
+	}
+}
+
+void mystery3(auto& D)
+{
+	cout<<endl<<"Mystery 3"<<endl<<"---------------------"<<endl;
+	
+	for (unsigned int i = 0; i < D.size(); i++)
+	{
+	for (unsigned int j = i; j < D.size(); j++)
+	if (D[j] < D[i])
+		swap (D[i], D[j]);
+		
+		print(D);
+	}
+}
+	
 //... Other mysteries...
 
 int main()
